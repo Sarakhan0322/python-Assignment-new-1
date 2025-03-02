bf57b7e4261ea5461aa5dd9874913ac3a0a525be
Sarakhan0322 <sarakhan417417@gmail.com>
2025-03-02 16:01:09 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+print("Hello, world!")
+# Pycache Example
+def greet(name):
+    return f"Hello, {name}!"
+
+print(greet("ARSALA RANA"))
+
+
+# data types
+
+# string
+name = "ARSALA RANA"
+
+# integer
+age = 29
+
+# float
+height = 5.3
+
+# boolean
+is_student = True
+
+# list
+grades = [95, 83, 90, 87, 74]
+
+# tuple
+coordinates = (3, 5)
+
+# dictionary
+person = {"name": "ARSALA RANA", "age": 29, "height": 5.3}
+
+# set
+unique_numbers = {1, 2, 3, 4, 5}
+
+print(name, age, height, is_student, grades, coordinates, person, unique_numbers)
